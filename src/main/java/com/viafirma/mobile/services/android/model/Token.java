f5e0f14a9784c80b686339405f8d84e46c1cf512
Ernesto Lage <ernesto.lage@gmail.com>
2022-10-10 08:56:16 +0200

    Fixed MSToken object

--- src/main/java/com/viafirma/mobile/services/android/model/Token.java
+++ src/main/java/com/viafirma/mobile/services/android/model/Token.java
@@ -9,18 +9,31 @@ import java.io.Serializable;
 @ApiModel(description = "")
 public class Token  implements Serializable {
   
-  @SerializedName("link")
-  private String link = null;
+  @SerializedName("oauth_token")
+  private String oauth_token = null;
+  @SerializedName("oauth_token_secret")
+  private String oauth_token_secret = null;
 
   
   /**
    **/
-  @ApiModelProperty(required = true, value = "")
-  public String getLink() {
-    return link;
+  @ApiModelProperty(required = false, value = "")
+  public String getOauth_token() {
+    return oauth_token;
   }
-  public void setLink(String link) {
-    this.link = link;
+  public void setOauth_token(String oauth_token) {
+    this.oauth_token = oauth_token;
+  }
+
+  
+  /**
+   **/
+  @ApiModelProperty(required = false, value = "")
+  public String getOauth_token_secret() {
+    return oauth_token_secret;
+  }
+  public void setOauth_token_secret(String oauth_token_secret) {
+    this.oauth_token_secret = oauth_token_secret;
   }
 
   
@@ -30,7 +43,8 @@ public class Token  implements Serializable {
     StringBuilder sb = new StringBuilder();
     sb.append("class Token {\n");
     
-    sb.append("  link: ").append(link).append("\n");
+    sb.append("  oauth_token: ").append(oauth_token).append("\n");
+    sb.append("  oauth_token_secret: ").append(oauth_token_secret).append("\n");
     sb.append("}\n");
     return sb.toString();
   }
